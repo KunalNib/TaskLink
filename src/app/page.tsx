7f294afd6dd35211630d20127086c64071ca2c8d
Kunal Nibrad <kunalnibrad297@gmail.com>
2026-09-18 23:18:19 +0530

    trpc setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,18 @@
-'use client';
+// 'use client';
 import { Button } from "@/components/ui/button";
-import { useState } from "react";
+import { caller } from "@/trpc/server";
+// import { useState } from "react";
 
-const Page =()=>{
+const Page =async()=>{
+
+  const users=await caller.getUsers();
 
   return (
     <div className=" min-h-screen min-w-screen  flex justify-center items-center">
-      <Button className="bg-black text-white" > Click Me</Button>
+      {/* <Button className="bg-black text-white" > Click Me</Button>
+      {JSON.stringify(users)} */}
+
+      Hey There
     </div> 
 
   )
