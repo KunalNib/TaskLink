7a82c0a919f0925f8f9e1a1f5b0effe11fd29e59
Kunal Nibrad <kunalnibrad297@gmail.com>
2026-09-19 23:40:29 +0530

    auth setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,25 @@
 // 'use client';
-import { Button } from "@/components/ui/button";
-import { caller } from "@/trpc/server";
+import { getQueryClient ,trpc} from "@/trpc/server";
+import { dehydrate, HydrationBoundary } from "@tanstack/react-query";
+import { Client } from "./client";
+import { Suspense } from "react";
 // import { useState } from "react";
 
 const Page =async()=>{
+  const queryClient=getQueryClient();
 
-  const users=await caller.getUsers();
+  void queryClient.query(trpc.getUsers.queryOptions());
 
   return (
     <div className=" min-h-screen min-w-screen  flex justify-center items-center">
       {/* <Button className="bg-black text-white" > Click Me</Button>
       {JSON.stringify(users)} */}
-
-      Hey There
+      <HydrationBoundary state={dehydrate(queryClient)}>
+        <Suspense fallback={<div>Loading...</div>}>
+          <Client  />
+        </Suspense>
+      </HydrationBoundary>
+      
     </div> 
 
   )
